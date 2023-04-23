4a413bf005f390d210f3b638109d596e74530258
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-04-23 21:06:44 +0300

    dashboard added

---
+++ TraversalCoreProject/ViewComponents/AdminDashboard/_AdminGuideList.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.ViewComponents.AdminDashboard
+{
+    public class _AdminGuideList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
