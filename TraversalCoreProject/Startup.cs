704cbc6d4bb67e86cf6a4092f32b9f6001ec7c08
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-04-26 23:54:53 +0300

    add error pages

--- TraversalCoreProject/Startup.cs
+++ TraversalCoreProject/Startup.cs
@@ -74,6 +74,7 @@ namespace TraversalCoreProject
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404/","?code={0}");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseAuthentication();
