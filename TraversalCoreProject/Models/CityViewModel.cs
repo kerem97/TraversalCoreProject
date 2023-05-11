72ca706bb4c5003c7fab78a5e9d34abfdf2218b4
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-05-11 23:35:10 +0300

    Ajax added

---
+++ TraversalCoreProject/Models/CityViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.Models
+{
+    public class CityViewModel
+    {
+        public int CityID { get; set; }
+        public string CityName { get; set; }
+        public string CityCountry { get; set; }
+    }
+}
