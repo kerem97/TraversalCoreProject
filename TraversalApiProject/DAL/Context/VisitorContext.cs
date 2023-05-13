e762c6ee2ee95dbce3c1fd09ec884a80b403fd08
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-05-13 23:32:49 +0300

    updated

---
+++ TraversalApiProject/DAL/Context/VisitorContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TraversalApiProject.DAL.Entities;
+
+namespace TraversalApiProject.DAL.Context
+{
+    public class VisitorContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-1MSR6CD\\SQLEXPRESS;initial catalog=TraversalDbApi;integrated security=true");
+
+        }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
