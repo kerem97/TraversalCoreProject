aced9eeb6306dabcda8d8dd3aa89a2867cc8c548
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-05-23 01:39:09 +0300

    updated

---
+++ TraversalCoreProject/Controllers/PasswordChangeController.cs
@@ -0,0 +1,37 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class PasswordChangeController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public PasswordChangeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ForgetPassword(ForgetPasswordViewModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Mail);
+
+            string passwordResetToken = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+            return View();
+        }
+    }
+}
--- TraversalCoreProject/Startup.cs
+++ TraversalCoreProject/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -80,13 +81,20 @@ namespace TraversalCoreProject
                    .Build();
                    config.Filters.Add(new AuthorizeFilter(policy));
                });
-            services.AddMvc();
 
-       
+            services.AddLocalization(opt =>
+            {
+                opt.ResourcesPath = "Resources";
+            });
+
+
+            services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
+
+
 
             services.ConfigureApplicationCookie(options =>
             {
-              
+
                 options.LoginPath = "/Login/SignIn/";
             });
 
@@ -113,6 +121,11 @@ namespace TraversalCoreProject
             app.UseRouting();
 
             app.UseAuthorization();
+
+            var supportedCultures = new[] { "en", "fr", "es", "gr", "tr", "de" };
+            var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[1]).AddSupportedCultures
+                (supportedCultures).AddSupportedUICultures(supportedCultures);
+            app.UseRequestLocalization(localizationOptions);
 
             app.UseEndpoints(endpoints =>
             {
